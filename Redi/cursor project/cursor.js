bb685cf2c7b46613c549d4d93f299e3e35918ec1
srujana2131 <srujana222014@gmail.com>
2024-05-07 17:46:17 +0200

    counterValidation

--- Redi/cursor project/cursor.js
+++ Redi/cursor project/cursor.js
@@ -17,20 +17,49 @@ let decreaseBtn = document.getElementById("decrement_button");
 let increaseBtn= document.getElementById("increment_button");
 let counter = document.getElementById("output");
 let clear= document.getElementById("reset");
+let msg = "You have free shipping";
+let message = document.getElementById("msg").value;
 let count =0;
 decreaseBtn.addEventListener('click', ()=>{
-    count--;
-    counter.innerHTML=count;
-    //counterStyle();
+    
+    if (count == 0) {
+        decreaseBtn.disabled = true;
+        counter.innerHTML=count;
+        
+        
+    }else if(count>0){
+        decreaseBtn.disabled = false;
+        count--;
+        counter.innerHTML=count;
+        if(count == 9){
+            document.getElementById("increment_button").style.backgroundColor='rgb(0 0 0 / 7%)';
+            document.getElementById('msg').style.display='none';
+        }else if(count == 19){
+            document.getElementById("increment_button").style.backgroundColor='rgb(0 0 0 / 7%)';
+            document.getElementById("msg").innerHTML="You have free shipping";
+        }
+        
+} 
 })
 increaseBtn.addEventListener('click', ()=>{
     count++;
     counter.innerHTML=count;
-   // counterStyle();
+    decreaseBtn.disabled = false;
+    if(count==10){
+        document.getElementById('msg').style.display='block';
+        document.getElementById("msg").innerHTML="You have free shipping";
+        counter.innerHTML=count;
+    }
+    else if(count==20){
+        document.getElementById("increment_button").style.backgroundColor="red";
+        document.getElementById("msg").innerHTML="Out of stock";
+        counter.innerHTML=count;
+    }
 })
 
 clear.addEventListener('click', ()=>{
     count=0;
+    document.getElementById('msg').style.display='none';
     counter.innerHTML=count;
     
 })
